521ec053d098fcd26387b9269daf25fc66a6bac7
= <=>
2019-05-21 19:55:30 +0200

    merging with oscar

--- blockchain/html/crypto.js
+++ blockchain/html/crypto.js
@@ -2,6 +2,15 @@ function sha256(plaintext) {
     return sjcl.codec.hex.fromBits(sjcl.hash.sha256.hash(plaintext))
 }
 
-function sign() {
+function generateKeyPair() {
+    let pair = sjcl.ecc.ecdsa.generateKeys(256)
+
+    return {
+        public: ,
+        private:,
+    }
+}
+
+function sign(plaintext, privateKey) {
 
 }
